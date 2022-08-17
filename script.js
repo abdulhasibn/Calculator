663364340f1098024cc8c2848d234ca190b1f696
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-17 19:26:04 +0530

    Add ids to the buttons

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function product(a,b){
 function divide(a,b){
     return a / b;
 }
+function operate(str, a, b){
+    if (str === "+"){
+        return add(a,b);
+    } else if (str === "-"){
+        return sub(a,b);
+    } else if (str === "*"){
+        return product(a,b);
+    } else if (str === "/"){
+        return divide(a,b);
+    } 
+}
+console.log(operate("+", 56, 101));
