8f160c9a13f92f5bef42c24923625335ff6b761f
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-19 08:01:54 +0530

    Format the code and add diplay on click feature

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-function add(a,b){
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
-function sub(a,b){
-    return a - b;
+function sub(a, b) {
+  return a - b;
 }
-function product(a,b){
-    return a * b;
+function product(a, b) {
+  return a * b;
 }
-function divide(a,b){
-    return a / b;
+function divide(a, b) {
+  return a / b;
 }
-function operate(str, a, b){
-    if (str === "+"){
-        return add(a,b);
-    } else if (str === "-"){
-        return sub(a,b);
-    } else if (str === "*"){
-        return product(a,b);
-    } else if (str === "/"){
-        return divide(a,b);
-    } 
+function operate(str, a, b) {
+  if (str === "+") {
+    return add(a, b);
+  } else if (str === "-") {
+    return sub(a, b);
+  } else if (str === "*") {
+    return product(a, b);
+  } else if (str === "/") {
+    return divide(a, b);
+  }
 }
-console.log(operate("+", 56, 101));
+const buttons = document.querySelectorAll(".displayable");
+const display = document.getElementById("display");
+const inputDisplay = document.getElementById("input_value");
+const operatingDisplay = document.getElementById("operating_value");
+const allClear = document.getElementById("AC");
+const clear = document.getElementById("C");
+const operators = document.querySelectorAll(".operators");
+const equals = document.getElementById("equals");
+
+allClear.addEventListener("click", () => {
+  display.innerText = "";
+});
+
+clear.addEventListener("click", () => {});
+
+for (const button of buttons) {
+  button.addEventListener("click", () => {
+    inputDisplay.innerText += button.innerText;
+    const valueOne = inputDisplay.innerText;
+  });
+}
+
+for (const operator of operators) {
+  operator.addEventListener("click", () => {
+    const operatorOne = operator.innerText;
+    operatingDisplay.innerText += inputDisplay.innerText;
+  });
+}
